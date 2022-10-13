81571a912773c89b732a45070bc47fc64f6c1a2f
Cory Cunningham <Corycunningham2010@gmail.com>
2022-10-13 10:13:31 -0400

    👌 IMPROVE: day 1

--- src/1/bandNameGenerator.py
+++ src/1/bandNameGenerator.py
@@ -1,5 +1,5 @@
 print("Welcome to the Band Name Generator.")
-city = input("What city did you grow up in?\n")
+city = input("Which city did you grow up in?\n")
 pet = input("What was the name of yor childhood pet?\n")
 
 print(f"Your band's name should be {city} {pet}.")
