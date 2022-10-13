422aae90089d6e91eb22f6b05db35e19a6b0b247
Cory Cunningham <Corycunningham2010@gmail.com>
2022-10-13 11:34:48 -0400

    👌 IMPROVE: tip calc

--- src/day-2/tip-calculator.py
+++ src/day-2/tip-calculator.py
@@ -2,6 +2,7 @@ print("Welcome to the tip calculator.")
 total_bill = float(input("What was the total? $"))
 num_of_people = int(input("How many people are splitting the bill? "))
 tip_percentage = float(input("What is the tip percentage? ")) / 100 + 1
-total_per_person = round((total_bill / num_of_people) * tip_percentage, 2)
+total_bill *= tip_percentage
+total_per_person = "{:.2f}".format(total_bill / num_of_people)
 
-print(f"Each person should pay ${total_per_person}.")
+print(f"Each person should pay: ${total_per_person}.")
